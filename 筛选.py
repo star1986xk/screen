0fea3bc34e8a4c02e398b638d7d62133930fc236
Administrator <173013905@qq.com>
2020-12-15 11:42:54 +0800

    调整

--- 筛选.py
+++ 筛选.py
@@ -2,7 +2,7 @@ import sys
 import re
 
 from UI.ui_win import Ui_Form
-from PyQt5.QtWidgets import QApplication, QFrame, QMessageBox
+from PyQt5.QtWidgets import QApplication, QFrame, QMessageBox, QTableWidgetItem
 from rsa_class import rsa_class
 
 
@@ -18,13 +18,18 @@ class MainWindow(QFrame, Ui_Form):
         self.rsa.Sig_auth.connect(self.close)
         self.rsa.start()
 
-        self.pushButton.clicked.connect(self.run)
+        self.pushButton_condition.clicked.connect(self.run_condition)
+        self.pushButton_chinese.clicked.connect(self.run_chinese)
 
-        self.var()
+        self.var_1()
+        self.var_2()
 
-    def var(self):
+    def var_1(self):
+        self.textBrowser.clear()
         self.result_true = []
         self.result_false = []
+
+    def var_2(self):
         self.chinese_true = []
         self.chinese_false = []
 
@@ -32,42 +37,55 @@ class MainWindow(QFrame, Ui_Form):
         QMessageBox.warning(self, '错误', '已过期')
         sys.exit(0)
 
-    def run(self):
+    def run_condition(self):
         if not self.textEdit.toPlainText() or not self.textEdit_2.toPlainText(): return
-        self.var()
+        self.var_1()
         text_list = [li.strip() for li in self.textEdit.toPlainText().split('\n') if li.strip()]  # type:list
         condition_list = [li.strip() for li in self.textEdit_2.toPlainText().split('\n') if li.strip()]  # type:list
-        condition_list = map(self.condition_create, condition_list)  # type:list
-        self.parser(text_list, condition_list)
+        condition_list = list(map(self.condition_create, condition_list))  # type:list
+        self.parser_condition(text_list, condition_list)
+
+    def run_chinese(self):
+        if not self.textEdit.toPlainText(): return
+        self.var_2()
+        text_list = [li.strip() for li in self.textEdit.toPlainText().split('\n') if li.strip()]  # type:list
+        self.parser_chinese(text_list)
 
     def condition_create(self, word):
-        if word[0] == '*' and word[-1] == '*':
-            return word.replace('*', '.*?')
-        elif word[0] != '*' and word[-1] == '*':
-            return '^' + word.replace('*', '.*?')
-        elif word[0] == '*' and word[-1] != '*':
-            return word.replace('*', '.*?') + '$'
+        if self.radioButton_in.isChecked():
+            condition = '.*?' + word + '.*?'
+        elif self.radioButton_start.isChecked():
+            condition = '^' + word + '.*?'
+        elif self.radioButton_end.isChecked():
+            condition = '.*?' + word + '$'
         else:
-            return '^' + word + '$'
+            condition = '^' + word + '$'
+        return condition, word
 
     def condition_parser(self, word, condition_list):
-        for c in condition_list:
+        for c, w in condition_list:
             if re.search(c, word):
-                return True
-        return False
+                return w
+        return None
 
-    def parser(self, text_list, condition_list):
+    def parser_condition(self, text_list, condition_list):
         for word in text_list:
-            if self.condition_parser(word, condition_list):
-                self.result_true.append(word)
+            c = self.condition_parser(word, condition_list)
+            if c:
+                self.result_true.append([word,c])
+                if self.checkBox.isChecked():
+                    self.textBrowser.append(word+'---'+c)
             else:
                 self.result_false.append(word)
+        self.textEdit_3.setText('\n'.join([li[0] for li in self.result_true]))
+        self.textEdit_4.setText('\n'.join(self.result_false))
+
+    def parser_chinese(self, text_list):
+        for word in text_list:
             if re.search('[\u4e00-\u9fa5]', word):
                 self.chinese_true.append(word)
             else:
                 self.chinese_false.append(word)
-        self.textEdit_3.setText('\n'.join(self.result_true))
-        self.textEdit_4.setText('\n'.join(self.result_false))
         self.textEdit_5.setText('\n'.join(self.chinese_true))
         self.textEdit_6.setText('\n'.join(self.chinese_false))
 
